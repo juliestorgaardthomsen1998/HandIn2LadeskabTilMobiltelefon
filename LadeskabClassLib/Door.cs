c0f3d785e0c3f00645173aac649c88d16012f8f9
Maria Samsing <au683916@uni.au.dk>
2022-03-13 10:42:14 +0100

    Tilføjelse af resterende klasser fra klassediagrammet.

---
+++ LadeskabClassLib/Door.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeskabClassLib
+{
+    public class DoorChangedEventArgs : EventArgs
+    {
+        public bool Door { get; set; }
+    }
+
+    public interface IDoor
+    {
+        event EventHandler<DoorChangedEventArgs> DoorChangedEvent;
+
+        public void LockDoor();
+    }
+
+    public class Door : IDoor
+    {
+        private bool oldDoorStatus;
+
+        public event EventHandler<DoorChangedEventArgs> DoorChangedEvent;
+
+        public void SetDoor(bool newDoorStatus)
+        {
+            if (newDoorStatus != oldDoorStatus)
+            {
+                OnDoorChanged(new DoorChangedEventArgs { Door = newDoorStatus });
+                oldDoorStatus = newDoorStatus;
+            }
+        }
+
+        protected virtual void OnDoorChanged(DoorChangedEventArgs e)
+        {
+            DoorChangedEvent?.Invoke(this, e);
+        }
+
+        public void LockDoor()
+        {
+
+        }
+    }
+
+   
+}
---
+++ LadeskabClassLib/RfidReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeskabClassLib
+{
+    public class RfidChangedEventArgs : EventArgs
+    {
+        public bool RfidReader { get; set; }
+    }
+    
+    public interface IRfidReader
+    {
+        event EventHandler<RfidChangedEventArgs> RfidChangedEvent;
+    }
+
+    public class RfidReader : IRfidReader
+    {
+        public event EventHandler<RfidChangedEventArgs> RfidChangedEvent;
+
+        public void SetRfid(bool newRfidStatus)
+        {
+
+        }
+    }
+}
